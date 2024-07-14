0858ee6b016234f62a1ca2d6fe4c90e350674583
Dilane F <dilanekamga777@gmail.com>
2024-07-14 12:34:38 +0400

    [CC-01]-build-your-own-wc-tool: 1. byteCount service 2. lineCount service 3. wordCount service 4. characterCount service

--- challenges/challenge-01-BuildYourOwn-wc-Tool/src/main/java/org/kamtech/Main.java
+++ challenges/challenge-01-BuildYourOwn-wc-Tool/src/main/java/org/kamtech/Main.java
@@ -1,10 +1,20 @@
 package org.kamtech;
 
+import org.kamtech.service.WCServices;
+
 /**
  * @author Dilane FOGUE KAMGA <dilanekamga777@gmail.com>
  */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    if (args.length < 1) {
+      System.out.println("Usage: ccwc [-c|-l|-w|-m] <filename>");
+      return;
+    }
+
+    var option = args.length == 2 ? args[0] : "";
+    var filePath = args.length == 2 ? args[1] : args[0];
+    var myServices = new WCServices(option);
+    myServices.myServices(filePath);
   }
 }
