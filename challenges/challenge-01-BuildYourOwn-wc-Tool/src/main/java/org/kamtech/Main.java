d258784a783a33495ffc0c3989f5f168a7f44aae
Dilane F <dilanekamga777@gmail.com>
2024-07-14 08:55:23 +0400

    Init challenge-01

---
+++ challenges/challenge-01-BuildYourOwn-wc-Tool/src/main/java/org/kamtech/Main.java
@@ -0,0 +1,10 @@
+package org.kamtech;
+
+/**
+ * @author Dilane FOGUE KAMGA <dilanekamga777@gmail.com>
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
